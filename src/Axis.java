57818cc9d1f1d95b534a2ef13a7144ffae5b9a40
Enrique Rodriguez <emrodriguez3e@gmail.com>
2021-03-14 22:49:25 -0700

    add more function to Axis

--- src/Axis.java
+++ src/Axis.java
@@ -3,6 +3,9 @@ public class Axis {
 
 	private double xAxis;
 	private double yAxis;
+	private double storage;
+	private double data;
+	private Boolean store;
 	
 	public double getxAxis() {
 		return xAxis;
@@ -10,12 +13,27 @@ public class Axis {
 	public void setxAxis(double xAxis) {
 		this.xAxis = xAxis;
 	}
+
 	public double getyAxis() {
 		return yAxis;
 	}
 	public void setyAxis(double yAxis) {
 		this.yAxis = yAxis;
 	}
+
+	public double getStorage(){
+		return storage;
+	}
+	public void setStorage(double storage){
+		this.storage = storage;
+	};
+
+	public double getData(){
+		return data;
+	}
+	public void setData(double data){
+		this.data = data;
+	}
 	
-	
+	public boolean isStorageType(){return store;}
 }
