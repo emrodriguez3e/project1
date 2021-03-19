27e28d2fad7b117ac1ec018b1f9a39299a66fa97
Enrique Rodriguez <emrodriguez3e@gmail.com>
2021-03-19 15:04:55 -0700

    include dijksta

--- src/playground.java
+++ src/playground.java
@@ -5,9 +5,14 @@ public class playground {
     //TODO What is dijkstra's algorithm
 
     private Map<Integer, Axis> nodes = new LinkedHashMap<Integer, Axis>();
+    Map<Integer, Boolean> discovered = new HashMap<Integer,Boolean>();
+    Map<Integer, Boolean> explored = new HashMap<Integer, Boolean>();
+    Map<Integer, Integer> parent = new HashMap<Integer, Integer>();
+
     public List dataNodesList = new ArrayList<>();
     public List storageNodesList = new ArrayList<>();
-    Map<Integer, Boolean> discovered = new HashMap<Integer,Boolean>();
+
+
     double electric = Math.pow(100, -12);
     double amp = Math.pow(100, -9);
     double k = 3200;
@@ -42,7 +47,7 @@ public class playground {
         if(allData > allStorage){
             System.out.println("There is not enough storage available to store all the data.\nPlease try again.");
             System.exit(1);
-        }
+        }//feasability check
 
         playground play = new playground();
         play.populateNodes(numberOfNodes,width, height, dataNodes); //this will give the location of the nodes
@@ -77,7 +82,8 @@ public class playground {
 
         for(int node: adjList.keySet()){
             Set<Integer> connectedNode = new LinkedHashSet<Integer>();
-//            stackDFS(node, connectedNode, adjList);
+            stackDFS(node, connectedNode, adjList);
+
 
             if(!connectedNode.isEmpty()){
                 connectedNodes.add(connectedNode);
@@ -90,10 +96,12 @@ public class playground {
             System.out.println("Graph is not fully connected.\nPlease try again.");
 
         }
-        System.out.println("\nID's of Data nodes: ");
-        System.out.println(dataNodesList);
-        System.out.println("\nID's of storage nodes: ");
-        System.out.println(storageNodesList);
+
+//        System.out.println("\nID's of Data nodes: ");//print out list with all DN
+//        System.out.println(dataNodesList);
+//        System.out.println("\nID's of storage nodes: ");//print out the list with all SN
+//        System.out.println(storageNodesList);
+
 
 
         System.out.println("There are " + connectedNodes.size() + " connected components");
@@ -124,6 +132,41 @@ public class playground {
             }
         }
     }
+
+    void DijkstraAlgorithm(int u, Map<Integer, Set<Integer>> adjList) {
+        int cost = 0;
+        int count = 0;
+        discovered.put(u, false);
+        explored.put(u, false);
+
+        //maybe change the list to a normal list instead of a list containing a set?
+        //List<Set<Integer>> pathCost = new ArrayList<Set<Integer>>();
+        List<Set<Integer>> connectedNodes = new ArrayList<Set<Integer>>();
+        //create a node for every adjList
+        for (int node : adjList.keySet()) {
+            Set<Integer> connectedNode = new LinkedHashSet<Integer>();
+            if (!connectedNode.isEmpty()) {
+                connectedNodes.add(connectedNode);
+            }
+            for (int j : adjList.get(count)) {
+                if (count == 0) {
+                    cost = 0;
+                    cost++;
+                } else if (count != 0) {
+                    cost = 1;
+                    cost++;
+                }
+                if (connectedNode == parent) {
+                    discovered.put(u, true);
+                    explored.put(u, true);
+                }
+                if (connectedNode != parent) {
+                    discovered.put(u, true);
+                    explored.put(u, false);
+                }
+            }
+        }
+    }//end of dijkstra
 
     void populateNodes(int nodeCount, double width, double height, int dataNode) {
         Random random = new Random();
@@ -180,13 +223,11 @@ public class playground {
 
 
                 //if a node is within range and has a data type
-                if(distance <= tr) {
 
                     double edge = electric * k + amp * k * Math.pow(distance,2) + electric *k;
 
-                    //node 1 is DN && node 2 is SN
-                    if(axis1.isStorageType() == false && axis2.isStorageType() == true){
-                        Set<Integer> tmpList = adjList.get(node1);
+                    //place all nodes into the adj list
+                    if(distance <= tr) {
                         Set<Integer> tempList = adjList.get(node1);
                         tempList.add(node2);
                         adjList.put(node1, tempList);
@@ -195,20 +236,7 @@ public class playground {
                         tempList.add(node1);
                         adjList.put(node2, tempList);
 
-                    }
-                    //node 1 is SN && node 2 is DN
-                    else if(axis1.isStorageType() == true && axis2.isStorageType() == false){
-                        Set<Integer> tmpList = adjList.get(node2);
-
-                        tmpList.add(node1);
-                        adjList.put(node2, tmpList);
-
-                        tmpList = adjList.get(node1);
-                        tmpList.add(node2);
-                        adjList.put(node1, tmpList);
-                    }//end of else if
-
-                }
+                }//end of distance
             }
         }
     }//end of adjList
